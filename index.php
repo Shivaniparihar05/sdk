cff59299913f4231cc41970f05dcdac2682b6842
Shivaniparihar05 <shivani.parihar05@gmail.com>
2020-10-29 20:05:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,70 @@
 <?php
-
-$secret = 'your-env-secret';
-
-$requestMethod = $_SERVER['REQUEST_METHOD'] ?? '';
-$receivedHmac = $_SERVER['HTTP_X_CS_SIGNATURE'] ?? '';
-$timestamp = $_SERVER['HTTP_X_CS_TIMESTAMP'] ?? '';
-$url = $_SERVER['REQUEST_URI'];
-$body = file_get_contents('php://input');
-
-$data = $requestMethod . $url . $timestamp . $body;
-$hmac = hash_hmac('sha256', $data, $secret);
-
-$isValid = hash_equals($hmac, $receivedHmac);
-
-if (!$isValid) {
-    // Invalid webhook signature, ignore the request.
-    die();
+ 
+// This should be replaced with your actual Partner Secret, or if signing using a User Key, the secret for that key.
+$SIGNINGKEYSECRET = "adhjshdsdjcnnc";
+ 
+ 
+// PHP converts the header into all UPPERCASE, converts hyphens("-") into underscores("_") and prepends "HTTP_"
+// or "HTTPS_" to the front, based on the protocol used.
+// Keep this in mind if converting this code to work in a language other than PHP. 
+// The actual header passed by Gigya is: "X-Gigya-Sig-Hmac-Sha1".
+$msgHash = $_SERVER['HTTP_X_GIGYA_SIG_HMAC_SHA1']; // How PHP sees the X-Gigya-Sig-Hmac-Sha1 header
+ 
+ 
+// Get the JSON payload sent by Gigya.
+$messageJSON = file_get_contents('php://input');
+ 
+ 
+// Decode the JSON payload into an associative array.
+$jsonDecoded = json_decode($messageJSON, TRUE);
+ 
+ 
+// Builds and returns expected hash
+function createMessageHash($secret, $message){
+    return base64_encode(hash_hmac('sha1', $message, base64_decode($secret), true));
+}
+ 
+ 
+// Compares the two parameters (in this case the hashes) and returns TRUE if they match
+// and FALSE if they don't.  
+function hashesMatch($expected, $received){
+    if ($expected == $received) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
-$webhook = json_decode($body);
+ 
+// Check if the hash matches. If it doesn't, it could mean that the data was tampered
+// with in flight. If so, do not send 2XX SUCCESS - let Gigya re-send the notification.
+if (hashesMatch(createMessageHash($SIGNINGKEYSECRET, $messageJSON), $msgHash)) {
+  
+    // Loop through the events portion of the notification.
+    for ($x=0; $x<sizeof($jsonDecoded['events']); $x++ ){
+    
+    $curEvt = $jsonDecoded['events'][$x]['type'];
+    $curUID = $jsonDecoded['events'][$x]['data']['uid'];
+    file_put_contents('php://stdout', 'Webhook event received: ' . print_r($curEvt, true) . "\r\n");
+    file_put_contents('php://stdout', 'Webhook event received: ' . print_r($curUID, true) . "\r\n");
+ 
+    /***************************************************************
+    ** This is where we would normally do something with this info.
+    ** For the sake of this example though, we'll just output
+    ** the info to the screen.
+    ***************************************************************/
+    echo "Event Type: $curEvt \n";
+    echo "UID: $curUID \n\n";
+    }
+    
+    // Since the hash is good and we've done what we need to do, respond OK. Gigya will not resend this notification.
+    http_response_code(200);
+ 
+} else {
+   
+    // The hash isn't good, respond non-OK. Gigya will try to resend this notification at progressively longer intervals.
+    http_response_code(400);
+}
+ 
+  file_put_contents('php://stdout', 'Webhook event received: ' . print_r($webhook, true) . "\r\n");
 
-file_put_contents('php://stdout', 'Webhook event received: ' . print_r($webhook, true) . "\r\n");
+?>
