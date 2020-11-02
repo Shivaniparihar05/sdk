1af8324547a000056eee7d2f199fa0a3e63a41f7
Shivaniparihar05 <shivani.parihar05@gmail.com>
2020-11-02 15:13:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
  
 // This should be replaced with your actual Partner Secret, or if signing using a User Key, the secret for that key.
-$SIGNINGKEYSECRET = "qKtasSfwcM3kdUX6QiZB4YTdZC2hCJJu";
+$SIGNINGKEYSECRET = "sxcbncnnxmmmmu";
  
  
 // PHP converts the header into all UPPERCASE, converts hyphens("-") into underscores("_") and prepends "HTTP_"
 // or "HTTPS_" to the front, based on the protocol used.
 // Keep this in mind if converting this code to work in a language other than PHP. 
 // The actual header passed by Gigya is: "X-Gigya-Sig-Hmac-Sha1".
-$msgHash = $_SERVER['HTTP_X_GIGYA_SIG_HMAC_SHA1']; // How PHP sees the X-Gigya-Sig-Hmac-Sha1 header
+$msgHash ="X-Gigya-Sig-Hmac-Sha1"; // How PHP sees the X-Gigya-Sig-Hmac-Sha1 header
  
  
 // Get the JSON payload sent by Gigya.
@@ -30,10 +30,12 @@ function createMessageHash($secret, $message){
 function hashesMatch($expected, $received){
     if ($expected == $received) {
         return TRUE;
+       
     }
     return FALSE;
+    
 }
-
+ 
  
 // Check if the hash matches. If it doesn't, it could mean that the data was tampered
 // with in flight. If so, do not send 2XX SUCCESS - let Gigya re-send the notification.
@@ -44,8 +46,6 @@ if (hashesMatch(createMessageHash($SIGNINGKEYSECRET, $messageJSON), $msgHash)) {
     
     $curEvt = $jsonDecoded['events'][$x]['type'];
     $curUID = $jsonDecoded['events'][$x]['data']['uid'];
-    file_put_contents('php://stdout', 'Webhook event received: ' . print_r($curEvt, true) . "\r\n");
-    file_put_contents('php://stdout', 'Webhook event received: ' . print_r($curUID, true) . "\r\n");
  
     /***************************************************************
     ** This is where we would normally do something with this info.
@@ -58,13 +58,14 @@ if (hashesMatch(createMessageHash($SIGNINGKEYSECRET, $messageJSON), $msgHash)) {
     
     // Since the hash is good and we've done what we need to do, respond OK. Gigya will not resend this notification.
     http_response_code(200);
+    echo "200";
  
 } else {
    
     // The hash isn't good, respond non-OK. Gigya will try to resend this notification at progressively longer intervals.
     http_response_code(400);
+    echo "400";
 }
  
-  file_put_contents('php://stdout', 'Webhook event received: ' . print_r($webhook, true) . "\r\n");
 
 ?>
